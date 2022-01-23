5a7403b1727ed931a04691bb69e22c258eda59d8
Jorge Badillo <badillo.delgado.jorge@gmail.com>
2022-01-23 11:22:42 -0600

    Solution for the Assignment 2 of R course. Two main functions, one to structure our process/store and other to calculate the inverse matrix or get the previously cached answer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For this assignment I've found the inv function from matlib library
+## The inv returns the inverse matrix (if exists)
+##I've followed the example structure of the vector
+##With these 2 functions one can calculate the inverse of a matrix and in
+##case of multiple calls with the same input our result is stored in the first call
 
-## Write a short comment describing this function
+library(matlib)
+
+## On this function multiple functions are defined inside, they will indicate us the value
+##.if it was previously calculated, check that all values, except the  first "m" are stored with <<-
+##This function returns a list that our next function will use
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## On this second function we check if we are calculating the same answer as last time it will call
+##of the list returned in the first function with the answer, if is different will calculate the inverse and
+##store it in the corresponding element of the first function lis return
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- inv(data, ...)
+  x$setinv(m)
+  m
 }
+A<-matrix( c(5, 1, 0,
+                           3,-1, 2,
+                           4, 0,-1), nrow=3, byrow=TRUE)
+B<-makeCacheMatrix(A)
+C<-cacheSolve(B)
+C %*% A
+AI %*% A
+B$get()
+C
+AI
+solve(A)
