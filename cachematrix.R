2202f3fd8f77bd94b4afa9c585e5a006e52e82ec
Jorge Badillo <badillo.delgado.jorge@gmail.com>
2022-01-23 11:25:16 -0600

    Added the same functions but with a "2" at the end of the names, these functions work with solve() instead of inv() to avoid missunderstandings

--- cachematrix.R
+++ cachematrix.R
@@ -41,11 +41,49 @@ cacheSolve <- function(x, ...) {
   x$setinv(m)
   m
 }
+
+## On this function multiple functions are defined inside, they will indicate us the value
+##.if it was previously calculated, check that all values, except the  first "m" are stored with <<-
+##This function returns a list that our next function will use
+
+makeCacheMatrix2 <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+## On this second function we check if we are calculating the same answer as last time it will call
+##of the list returned in the first function with the answer, if is different will calculate the inverse and
+##store it in the corresponding element of the first function lis return
+
+cacheSolve2 <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
+
 A<-matrix( c(5, 1, 0,
                            3,-1, 2,
                            4, 0,-1), nrow=3, byrow=TRUE)
-B<-makeCacheMatrix(A)
-C<-cacheSolve(B)
+B<-makeCacheMatrix2(A)
+C<-cacheSolve2(B)
 C %*% A
 AI %*% A
 B$get()
